3ba2ae22a7b95b6dacef5149a0eea36e086c0c3b
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-01-27 17:31:10 -0200

    Added Aiming behaviour

---
+++ Selfie/Assets/SelfieTargetPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieTargetPoint : MonoBehaviour {
+    public Bounds BoundingBox
+    { get { return GetComponent<Collider>().bounds; } }
+
+    public SelfieTarget parent;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    internal void OnStartedAiming()
+    {
+        parent.OnChildTargeted();
+    }
+
+    internal void OnstoppedAiming()
+    {
+        parent.OnChildUntargeted();
+    }
+}
