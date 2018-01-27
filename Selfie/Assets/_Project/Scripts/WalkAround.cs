3da26f0b52d95025609f0c6be73225373f9c577b
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-01-27 12:40:08 -0200

    Path Prototype

---
+++ Selfie/Assets/_Project/Scripts/WalkAround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkAround : MonoBehaviour {
+    public List<Transform> waypoints;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(waypoints[0].position);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
