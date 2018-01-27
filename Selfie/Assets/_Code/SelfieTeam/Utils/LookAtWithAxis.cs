87931d6ff2f6395e6dc973a78ad1d494b751a376
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-01-27 21:28:22 -0200

    LookAt

---
+++ Selfie/Assets/_Code/SelfieTeam/Utils/LookAtWithAxis.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SelfieTeam.Utils
+{
+    public class LookAtWithAxis : MonoBehaviour
+    {
+        public Vector3 axis;
+        public Transform target;
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            Plane plane = new Plane(axis, transform.position);
+
+            transform.rotation = Quaternion.LookRotation(plane.ClosestPointOnPlane(target.position) - transform.position);
+        }
+    }
+}
