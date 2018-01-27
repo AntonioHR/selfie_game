1352de843f9a3e4f6c6727dee7c670618b7ddbcc
Joao Lucas de Carvalho Arruda <jlcarruda3@gmail.com>
2018-01-27 16:21:41 -0200

    Path/Circuit System implemented

--- Selfie/Assets/_Project/Scripts/WalkAround.cs
+++ Selfie/Assets/_Project/Scripts/WalkAround.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class WalkAround : MonoBehaviour {
-    public List<Transform> waypoints;
+    public GameObject CircuitObject;
+    private List<Transform> waypoints;
+    private Circuit circuit;
     NavMeshAgent agent;
+    int destinationPoint = 0;
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
+        circuit = CircuitObject.GetComponent<Circuit>();
 
-        agent.SetDestination(waypoints[0].position);
+        waypoints = circuit.Waypoints;
+        agent.SetDestination(waypoints[destinationPoint].position);
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        agent.autoBraking = false;
+
+        this.GoToNextPoint();
 	}
+
+    void GoToNextPoint()
+    {
+        if (waypoints.Count == 0)
+        {
+            return;
+        } else if (Vector3.Distance(waypoints[destinationPoint].position, transform.position) < 1)
+        {
+            destinationPoint = (destinationPoint + 1) % waypoints.Count;
+            agent.SetDestination(waypoints[destinationPoint].position);
+        }
+
+
+    }
 }
