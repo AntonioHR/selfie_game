724a50c2ae962d3be4ef7fb5de9ad0fa6c7bdaf8
André Cardoso <amc.acardoso@gmail.com>
2018-01-28 12:15:21 -0300

    Menu Scene

---
+++ Selfie/Assets/_Code/SelfieTeam/Utils/DG/DGPunchScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DGPunchScale : MonoBehaviour {
+
+	public Vector3 punch;
+	public float duration;
+	public int vibrato;
+	[Range(0,1)]
+	public float elasticity;
+
+	public bool canTween = true;
+
+	public void PlayTween(){
+
+		if(canTween){
+			transform.DOComplete();
+			transform.DOPunchScale(punch,duration,vibrato, elasticity);
+		}
+
+	}
+
+
+}
---
+++ Selfie/Assets/_Code/SelfieTeam/Utils/DG/DGText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class DGText : MonoBehaviour {
+
+	public float duration;
+
+	// Use this for initialization
+	void Start () {
+
+		string txt = GetComponent<Text>().text;
+		GetComponent<Text>().DOText(txt, duration, true, ScrambleMode.Custom, " ");
+		
+	}
+}
