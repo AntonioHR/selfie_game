74ac82b7e22fae666796f2efc77e6d55b3bb6647
Antonio Harger Rivero <antonio.harger@gmail.com>
2018-01-28 14:48:37 -0200

    Follow Objectives

---
+++ Selfie/Assets/FlyingCube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Assets
+{
+    public class FlyingCube : MonoBehaviour
+    {
+        public Vector3 target;
+        public float time = 3;
+        private void Start()
+        {
+            transform.DOMove(transform.TransformPoint(target), time).SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+}
